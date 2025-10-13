622dc0cbea1f8811b33581c5e2bceac030f9dc8c
mmoya <maria.moya.g@ug.uchile.cl>
2025-10-13 20:40:43 -0300

    🎉 Inicializa proyecto Bonsai con estructura base, modelos, rutas y frontend

--- app.py
+++ app.py
@@ -107,7 +107,8 @@ def create_app():
 
     return app
 
+app = create_app() 
+
 if __name__ == "__main__":
-    app = create_app()
     port = int(os.environ.get("PORT", 5000))  # Render define PORT
     app.run(host="0.0.0.0", port=port, debug=True)
